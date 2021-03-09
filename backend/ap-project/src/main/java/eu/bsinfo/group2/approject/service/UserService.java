eb6e7e048e8eef9f0723bcefcb482a80ff310b26
Kevin Kraus <kevin.kraus1@outlook.com>
2021-03-09 15:45:46 +0100

    feature: fully working User entity with CRUD-REST-Controller located at /api/v1/user
    Signed-off-by: Kevin Kraus <kevin.kraus1@outlook.com>

---
+++ backend/ap-project/src/main/java/eu/bsinfo/group2/approject/service/UserService.java
@@ -0,0 +1,19 @@
+package eu.bsinfo.group2.approject.service;
+
+import eu.bsinfo.group2.approject.entities.user.UserDbo;
+import eu.bsinfo.group2.approject.exception.UserAlreadyExistsException;
+import eu.bsinfo.group2.approject.exception.UserNotFoundException;
+import eu.bsinfo.group2.approject.util.SuccessResult;
+
+import java.util.Optional;
+
+public interface UserService {
+
+    UserDbo createUser(UserDbo user) throws UserAlreadyExistsException;
+
+    UserDbo updateUser(String username, UserDbo user) throws UserNotFoundException;
+
+    SuccessResult deleteUser(String username) throws UserNotFoundException;
+
+    Optional<UserDbo> findUser(String username);
+}
---
+++ backend/ap-project/src/main/java/eu/bsinfo/group2/approject/web/v1/BaseAbstractController.java
@@ -0,0 +1,30 @@
+package eu.bsinfo.group2.approject.web.v1;
+
+import eu.bsinfo.group2.approject.exception.UnauthorizedException;
+import eu.bsinfo.group2.approject.exception.UserAlreadyExistsException;
+import eu.bsinfo.group2.approject.exception.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public abstract class BaseAbstractController {
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(UnauthorizedException.class)
+    public void handleUnauthorized() {
+    }
+
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(UserNotFoundException.class)
+    public void handleNotFound() {
+    }
+
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public void handleAlreadyExists() {
+    }
+
+}
