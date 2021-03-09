576cc7d56cec9fdf7a5f0e1a5099f397b077b553
Kevin Kraus <kevin.kraus1@outlook.com>
2021-03-09 10:44:27 +0100

    feature: basic setup

---
+++ backend/ap-project/src/main/java/eu/bsinfo/group2/approject/ApProjectApplication.java
@@ -0,0 +1,13 @@
+package eu.bsinfo.group2.approject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApProjectApplication.class, args);
+	}
+
+}
---
+++ backend/ap-project/src/test/java/eu/bsinfo/group2/approject/ApProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.bsinfo.group2.approject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
