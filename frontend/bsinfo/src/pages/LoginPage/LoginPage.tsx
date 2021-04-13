0427949d09d549781250d509c1e0d3fb7827f9d8
Kevin Kraus <kevin.kraus1@outlook.com>
2021-04-14 00:31:39 +0200

    feature: add switcher & styling
    Signed-off-by: Kevin Kraus <kevin.kraus1@outlook.com>

--- frontend/bsinfo/src/pages/LoginPage/LoginPage.tsx
+++ frontend/bsinfo/src/pages/LoginPage/LoginPage.tsx
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from "./LoginPage.module.scss"
 import {RegistrationForm} from "../../components/RegistrationForm/RegistrationForm";
+import {LoginForm} from "../../components/LoginForm/LoginForm";
+import Button from "react-bootstrap/Button"
 
 export function LoginPage() {
+
+    const [selectedMode, setSelectedMode] = useState("login")
+
+    function switchMode(e: any) {
+        if (selectedMode === "login") {
+            setSelectedMode("registration");
+        } else {
+            setSelectedMode("login");
+        }
+    }
+
     return (
         <div className={styles.page}>
             <div className={styles.form}>
+                <div className={styles.switcher}>
+                    <Button className={styles.switcher} variant="info" onClick={switchMode}>
+                        {selectedMode !== "login" ? "Zum Login" : "Zur Registrierung"}
+                    </Button>
+                </div>
+                {selectedMode === "login" &&
+                <LoginForm/>
+                }
+
+                {selectedMode === "registration" &&
                 <RegistrationForm/>
+                }
             </div>
         </div>
     )
