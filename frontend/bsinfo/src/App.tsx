456a2ae3797a2711f7b0f924a5fa08483ee5e605
Kevin Kraus <kevin.kraus1@outlook.com>
2021-04-16 00:20:20 +0200

    feature: routing & default behaviour if no type is selected
    Signed-off-by: Kevin Kraus <kevin.kraus1@outlook.com>

--- frontend/bsinfo/src/App.tsx
+++ frontend/bsinfo/src/App.tsx
@@ -2,12 +2,28 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {UserManagementPage} from "./pages/UserManagementPage/UserManagementPage";
-
+import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom"
+import {LoginPage} from "./pages/LoginPage/LoginPage";
 
 function App() {
-  return (
-      <UserManagementPage/>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route path={"/login"}>
+                    <LoginPage mode={"login"}/>
+                </Route>
+                <Route path={"/register"}>
+                    <LoginPage mode={"registration"}/>
+                </Route>
+                <Route path={"/manageUsers"}>
+                    <UserManagementPage/>
+                </Route>
+                <Route path={"/"}>
+                    <Redirect to={"/login"}/>
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
