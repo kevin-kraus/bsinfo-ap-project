b28fbab6c9f5fc00511264d5a2d001c1798d60d0
Kevin Kraus <kevin.kraus1@outlook.com>
2021-04-16 09:03:46 +0200

    feature: minimize CORS-boilerplate
    Signed-off-by: Kevin Kraus <kevin.kraus1@outlook.com>

--- backend/ap-project/src/main/java/eu/bsinfo/group2/approject/ApProjectApplication.java
+++ backend/ap-project/src/main/java/eu/bsinfo/group2/approject/ApProjectApplication.java
@@ -2,28 +2,12 @@ package eu.bsinfo.group2.approject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApProjectApplication.class, args);
-	}
-
-	/**
-	 * CORS-related configuration.
-	 */
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/api/v1/*").allowedOrigins("http://localhost:3000");
-			}
-		};
 	}
 
 }
